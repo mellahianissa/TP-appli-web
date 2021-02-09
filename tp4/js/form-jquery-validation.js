256f81c083c8dac4fc571f204b374d07dc6fa21c
mellahianissa <72705522+mellahianissa@users.noreply.github.com>
2021-02-09 14:02:25 +0100

    Update form-jquery-validation.js

--- tp4/js/form-jquery-validation.js
+++ tp4/js/form-jquery-validation.js
@@ -1,83 +1,91 @@
-
-  
-  
-  
-    /*  $( document ).ready(function() {
-            $("#valider").on("click",function(event){
-                var v_nom = $("#nom").val();
-                var v_prenom = $("#prenom").val();
-                var v_date =  $("#date").val();
-                var v_adresse = $("#adresse").val();
-                var v_mail = $("#mail").val();
-              
-          
-
-            
-            
-                if(v_nom !== "" && v_prenom !== "" && v_date !== "" && v_adresse !=="" && v_mail !== "") {
-
-                   
-                    
-                    $(".modal-title").html('Bienvenue ' + $("#nom").val() +' '+''+$("#prénom").val()+''+'Vous êtes née le ' + ''+ $("#date").val());
-                   
-                    $(".modal-body").html(`Vous habitez :
-                    <a class="link"  href="http://maps.google.com/maps?" ><img src="https://maps.googleapis.com/maps/api/staticmap?markers=${v_adresse}&zoom=14&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/> Lien Google Maps </a>`);
-                    $("#myModal").modal("show");
-                   
-   
-                   
-                }
-                else  if(v_nom == "" || v_prenom == "" || v_date == "" ||v_adresse =="" || v_mail == ""){
-                    $(".modal-title").html('Tout les champs sont obligatoire' );
-                    $("#myModal").modal("show");
-                }
-               });
-              
-            
-               
-
-              
-         });*/
-        
-         
-
-    
-
 $(document).ready(function () {
-    $("#gps").on("click",function(){ //ici la géolocalisation nous donnera la latitude et la la longitude de l’utilsateur
-        
-   
-    getLocation(); //L'API Géolocalisation HTML5  est utilisée pour obtenir la position géographique d'un utilisateur 
+
+    var test = 0;
+
+    $("#date").flatpickr({
+        dateFormat: "d-m-Y",
+        maxDate: "today"
     });
-  // Le script qui devra calculer et afficher le nombre de mots et de caractères
-      
-    $(document).keyup(function(e1) {
-   var nombreCaractere = $("#nom").val().length;
-        var msg = nombreCaractere + ' car';
-    
-        $('#compteur1').text(msg);
-    
-    
-        var nombreCaractere = $("#prénom").val().length;
-        var msg = nombreCaractere + ' car';
-    
-        $('#compteur2').text(msg);
 
-       
-        var nombreCaractere = $("#date").val().length;
-        var msg = nombreCaractere + ' car';
-        $('#compteur3').text(msg);
+    //realtime counter on Lastname
+    $("#lastname").keyup(function () {
+        var len = $(this).val().length;
+        var count = len + " Car."
+        $("#lastnm_counter").text(count);
 
-        var nombreCaractere4 = $("#adresse").val().length;
-        var msg = nombreCaractere + ' car';
-    
-        $('#compteur4').text(msg);
+    });
 
-        var nombreCaractere = $("#mail").val().length;
-        var msg = nombreCaractere + ' car';
-    
-        $('#compteur5').text(msg);
-      
-       
-     });
+    //realtime counter on Firstname
+    $("#firstname").keyup(function () {
+        var len = $(this).val().length;
+        var count = len + " Car."
+        $("#firstnm_counter").text(count);
+
+    });
+
+    $("#subbutton").click(function (event) {
+
+        event.preventDefault();
+
+
+
+        // for(var index in contactList){
+        //     console.log(contactList[index].name);
+        //   }
+        for (let item of $(".person")) {
+            if (item.value.length < 5) {
+
+                $('#myModal').modal("show");
+                $(".modal-title").text("Attention ! ");
+                $(".modal-body").html('Veuillez renseigner tous les champs s\'il vous plait');
+                test += 1;
+
+
+            } else {
+                test = 0;
+                $('#myModal').modal("show");
+                $(".modal-title").text("Bravo!  ");
+                $(".modal-body").html("Le formulaire est sauvegardé.");
+            }
+        }
+
+        //Vérifier si tous les champs sont remplis avant d'enregister un contact
+        if (test == 0) {
+            contactStore.add($("#lastname").val(), $("#firstname").val(), $("#date").val(), $("#address").val(), $("#mail").val());
+        }
+
+        //Rénitialiser le tableau avant chaque affichage
+        $("#tableContacts").html("");
+        $("#tableContacts").append(
+            "<thead>" +
+            "<tr>" +
+            "<th>Nom</th>" +
+            "<th>Prénom</th>" +
+            "<th>Date de naissance</th>" +
+            "<th>Adresse</th>" +
+            "<th>Mail</th>" +
+            "</tr>" +
+            "</thead>" +
+            "<tbody>" +
+            " </tbody>"
+
+        );
+
+        //Affichage de la liste des contacts
+        for (var i in contactStore.getList()) {
+            $("#tableContacts tbody").append(
+                "<tr>" +
+                "<td>" + contactStore.getList()[i].lastname + "</td>" +
+                "<td>" + contactStore.getList()[i].firstname + "</td>" +
+                "<td>" + contactStore.getList()[i].date + "</td>" +
+                "<td> <a href=\"https://maps.google.com/?q=" + contactStore.getList()[i].address + "\">" + contactStore.getList()[i].address + "</a> </td>" +
+                "<td> <a href=\"mailto:" + contactStore.getList()[i].mail + "\">" + contactStore.getList()[i].mail + "</a> </td>" +
+                "</tr>"
+
+            );
+        }
+
+
+
+    });
 });
