6f46b4949f908bc8e764ed912b7e722a007003b3
mellahianissa <72705522+mellahianissa@users.noreply.github.com>
2021-02-09 13:33:27 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -2,43 +2,41 @@
   
   
   
-        $( document ).ready(function() {
-            $("#valider").on("click",function(event){
-                var v_nom = $("#nom").val();
-                var v_prenom = $("#prenom").val();
-                var v_date =  $("#date").val();
-                var v_adresse = $("#adresse").val();
-                var v_mail = $("#mail").val();
-              
-          
+       $(document).ready(function () {
 
-            
-            
-                if(v_nom !== "" && v_prenom !== "" && v_date !== "" && v_adresse !=="" && v_mail !== "") {
+    $("#date").flatpickr({
+        dateFormat: "d-m-Y",
+        maxDate: "today"
+    });
 
-                   
-                    
-                    $(".modal-title").html('Bienvenue ' + $("#nom").val() +' '+''+$("#prénom").val()+''+'  vous êtes née le  ' + ''+''+ $("#date").val());
-                   
-                    $(".modal-body").html(`Vous habitez :
-                    <a class="link"  href="http://maps.google.com/maps?" ><img src="https://maps.googleapis.com/maps/api/staticmap?markers=${v_adresse}&zoom=14&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/> Lien Google Maps </a>`);
-                    $("#myModal").modal("show");
-                   
-   
-                   
-                }
-                else  if(v_nom == "" || v_prenom == "" || v_date == "" ||v_adresse =="" || v_mail == ""){
-                    $(".modal-title").html('Tout les champs sont obligatoire' );
-                    $("#myModal").modal("show");
-                }
-               });
-              
-            
-               
+    $("#subbutton").click(function (event) {
 
-              
-         });
-        
+        event.preventDefault();
 
 
- 
+        for (let item of $(".person")) {
+            if (item.value.length < 5) {
+                
+                $('#myModal').modal("show");
+                $(".modal-title").text("Attention ! ");
+                $(".modal-body").html('Veuillez renseigner tous les champs s\'il vous plait');
+
+            } else {
+
+                $('#myModal').modal("show");
+                $(".modal-title").text("Bienvenue " + $("#firstname").val() + " 👋");
+
+                var city = $("#address").val();
+                var date = $("#date").val();
+
+                $(".modal-body").html(`Vous êtes nés le ${date} et vous habitez :
+                <br>
+                <br>
+                <a class="link text-right"  href="http://maps.google.com/maps?" >
+                <img src="https://maps.googleapis.com/maps/api/staticmap?markers=${city}&zoom=14&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/> Google Map </a>`
+                );
+
+            }
+        }
+    });
+});
